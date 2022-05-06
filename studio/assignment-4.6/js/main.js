f525eb9f667faefae3b779037fcb67b8f880c868
lakeisha <sharl281@newschool.edu>
2022-05-05 20:53:53 -0400

    help pls

--- studio/assignment-4.6/js/main.js
+++ studio/assignment-4.6/js/main.js
@@ -1,11 +1,31 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('carousel-slide img');
+// const carouselSlide = document.querySelector('.carousel-slide');
+// const carouselImages = document.querySelectorAll('carousel-slide img');
 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+// const prevBtn = document.querySelector('#prevBtn');
+// const nextBtn = document.querySelector('#nextBtn');
 
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+// let counter = 1;
+// const size = carouselImages[0].clientWidth;
 
 
 
+// document.getElementById("but").addEventListener(
+//     "click",
+//     function(event) {
+//       if (event.target.value === "Look In Mirror") {
+//         event.target.value = "Choose A Hairstyle";
+//       } else {
+//         event.target.value = "Look In Mirror";
+//       }
+//     },
+//     false
+//   );
+
+function changeImage(){
+    let displayImage = document.getElementById('image1')
+    if (displayImage.src.match('./images/wallpaper-2.jpg')){
+            displayImage.src = './images/wallpaper-3.jpeg'
+    } else {
+        displayImage.src = './images/wallpaper-2.jpg'
+    }
+}
