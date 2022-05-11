5fda7e327234cd6f831d7704919d5a8a75f7f039
lakeisha <sharl281@newschool.edu>
2022-05-11 16:02:02 -0400

    hi

--- studio/assignment-4.6/js/main.js
+++ studio/assignment-4.6/js/main.js
@@ -21,14 +21,28 @@
 //     false
 //   );
 
+
+
 function changeImage(){
+    // console.log('here')
     let displayImage = document.getElementById('image1')
     if (displayImage.src.includes('/images/wallpaper-2.jpg')){
             displayImage.src = './images/wallpaper-3.jpeg'
+            displayImage.src = './images/wallpaper-4.jpg' //??//
+            displayImage.src = './images/wallpaper-5.jpg'
+            displayImage.src = './images/wallpaper-6.png'
     } else {
         displayImage.src = './images/wallpaper-2.jpg'
     }
-
     console.log(displayImage.src);
 }
 
+
+function show() {
+    document.getElementById('slideshow-container')
+        .style.display = "block";
+    document.getElementById('but')
+        .style.display = "block";
+    document.getElementById('arrows')
+        .style.display= "block";
+}
